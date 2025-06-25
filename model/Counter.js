5435666ccbf7df44b4f61936f6525c6385af4b6c
RiponHassan456 <riponhassan456700@gmail.com>
2025-06-25 21:18:49 +0600

    Delete model directory

--- model/Counter.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const counterSchema = new mongoose.Schema({
-  id: String,
-  seq: Number
-});
-
-module.exports = mongoose.model('Counter', counterSchema);
--- model/categoryModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const categorySchema = new mongoose.Schema({
-  catName: { type: String, required: true }
-});
-const Category = mongoose.model('Category', categorySchema);
-module.exports = Category;
